dcc2bd0e4a8fd8db7c7549030bb667fc6292d5f1
wkdgmr <wkdoov@gmail.com>
2023-04-02 15:24:09 -0400

    Shared XP fix reference to player.plractive

--- Source/player.cpp
+++ Source/player.cpp
@@ -2541,7 +2541,7 @@ void AddPlrMonstExper(int lvl, int exp, char pmask)
 
 	if (totplrs != 0) {
 		int e = exp / totplrs;
-		AddPlrExperience(Player &player, lvl, e);
+		AddPlrExperience(player.plractive, lvl, e);
 	}
 }
 
