9e28131671b6a0eb6c6001f320c81dfa285e31dc
wkdgmr <wkdoov@gmail.com>
2023-04-02 14:47:40 -0400

    fixing Shared XP function

--- Source/player.cpp
+++ Source/player.cpp
@@ -2541,7 +2541,7 @@ void AddPlrMonstExper(int lvl, int exp, char pmask)
 
 	if (totplrs != 0) {
 		int e = exp / totplrs;
-		AddPlrExperience(MyPlayerId, lvl, e);
+		AddPlrExperience(MyPlayer, lvl, e);
 	}
 }
 
