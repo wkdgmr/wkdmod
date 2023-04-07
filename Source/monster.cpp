0b48f36451c192e9a027c856d1d51535815db31b
wkdgmr <wkdoov@gmail.com>
2023-04-07 12:34:52 -0400

    testing Defiler ring drop

--- Source/monster.cpp
+++ Source/monster.cpp
@@ -885,9 +885,9 @@ void DiabloDeath(Monster &diablo, bool sendmsg)
 			CreateTypeItem(monster.position.tile + Displacement { 1, 1 }, true, ItemType::Mace, IMISC_NONE, sendmsg, false);
 		} else if (monster.uniqueType == UniqueMonsterType::Defiler) {
 			if (sgGameInitInfo.nDifficulty == DIFF_NORMAL) {
-				CreateMagicWeapon(monster.position.tile, ItemType::Helm, ICURS_FULL_HELM, sendmsg, false);
-				CreateMagicWeapon(monster.position.tile, ItemType::LightArmor, ICURS_STUDDED_LEATHER_ARMOR, sendmsg, false);
-				CreateMagicWeapon(monster.position.tile, ItemType::Shield, ICURS_LARGE_SHIELD, sendmsg, false);
+				CreateMagicWeapon(monster.position.tile, ItemType::Ring, ICURS_RING, sendmsg, false);
+				CreateMagicWeapon(monster.position.tile, ItemType::Ring, ICURS_RING, sendmsg, false);
+				CreateMagicWeapon(monster.position.tile, ItemType::Ring, ICURS_RING, sendmsg, false);
 			} else if (sgGameInitInfo.nDifficulty == DIFF_NIGHTMARE) {
 				CreateMagicWeapon(monster.position.tile, ItemType::Helm, ICURS_CROWN, sendmsg, false);
 				CreateMagicWeapon(monster.position.tile, ItemType::MediumArmor, ICURS_SPLINT_MAIL, sendmsg, false);
