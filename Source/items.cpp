d93a550217f1c8f8662608c5b60dba7950970f4d
wkdgmr <wkdoov@gmail.com>
2023-04-15 02:36:15 -0400

    Fixing Monk buffs

--- Source/items.cpp
+++ Source/items.cpp
@@ -2841,44 +2841,44 @@ void CalcPlrItemVals(Player &player, bool loadgfx)
 	} else if (player.InvBody[INVLOC_CHEST]._itype == ItemType::MediumArmor) {
 		if (player._pClass == HeroClass::Monk)
 			if (sgGameInitInfo.nDifficulty == DIFF_NORMAL) {
-				player._pIAC += player._pIAC * 2;
+				player._pIAC += player._pIAC;
 				player._pIBonusToHit += 20;
-				player._pMagResist += 15;
-				player._pFireResist += 15;
-				player._pLghtResist += 15;
+				player._pMagResist += 10;
+				player._pFireResist += 10;
+				player._pLghtResist += 10;
+			} else if (sgGameInitInfo.nDifficulty == DIFF_NIGHTMARE) {
+				player._pIAC += player._pIAC * 2;
+				player._pIBonusToHit += 50;
+				player._pMagResist += 20;
+				player._pFireResist += 20;
+				player._pLghtResist += 20;
 			} else if (sgGameInitInfo.nDifficulty == DIFF_NIGHTMARE) {
 				player._pIAC += player._pIAC * 4;
-				player._pIBonusToHit += 50;
-				player._pMagResist += 25;
-				player._pFireResist += 25;
-				player._pLghtResist += 25;
-			} else if (sgGameInitInfo.nDifficulty == DIFF_NIGHTMARE) {
-				player._pIAC += player._pIAC * 8;
 				player._pIBonusToHit += 100;
-				player._pMagResist += 35;
-				player._pFireResist += 35;
-				player._pLghtResist += 35;
+				player._pMagResist += 30;
+				player._pFireResist += 30;
+				player._pLghtResist += 30;
 			}
 		animArmorId = PlayerArmorGraphic::Medium;
-	} else if (player._pClass == HeroClass::Monk) {
+	} else if (player.InvBody[INVLOC_CHEST]._itype == ItemType::LightArmor) {
 		if (sgGameInitInfo.nDifficulty == DIFF_NORMAL) {
-			player._pIAC += player._pIAC * 3;
+			player._pIAC += player._pIAC * 2;
 			player._pIBonusToHit += 20;
-			player._pMagResist += 15;
-			player._pFireResist += 15;
-			player._pLghtResist += 15;
+			player._pMagResist += 10;
+			player._pFireResist += 10;
+			player._pLghtResist += 10;
 		} else if (sgGameInitInfo.nDifficulty == DIFF_NIGHTMARE) {
-			player._pIAC += player._pIAC * 6;
+			player._pIAC += player._pIAC * 4;
 			player._pIBonusToHit += 50;
-			player._pMagResist += 25;
-			player._pFireResist += 25;
-			player._pLghtResist += 25;
+			player._pMagResist += 20;
+			player._pFireResist += 20;
+			player._pLghtResist += 20;
 		} else if (sgGameInitInfo.nDifficulty == DIFF_NIGHTMARE) {
-			player._pIAC += player._pIAC * 12;
+			player._pIAC += player._pIAC * 8;
 			player._pIBonusToHit += 100;
-			player._pMagResist += 35;
-			player._pFireResist += 35;
-			player._pLghtResist += 35;
+			player._pMagResist += 30;
+			player._pFireResist += 30;
+			player._pLghtResist += 30;
 		}
 	}
 
