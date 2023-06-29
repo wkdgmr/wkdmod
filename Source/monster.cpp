7f0af53823a3708e5e9ecf79fbb6762fae443da8
wkdgmr <wkdoov@gmail.com>
2023-06-29 16:17:20 -0400

    Holy Fire damage type set to Fire

--- Source/monster.cpp
+++ Source/monster.cpp
@@ -1253,7 +1253,10 @@ void MonsterAttackPlayer(Monster &monster, Player &player, int hit, int minDam, 
 			eMind = player._pIFMinDam;
 			eMaxd = player._pIFMaxDam;
 			int mdam = (GenerateRnd(eMaxd) + eMind) << 6;
-			ApplyMonsterDamage(DamageType::Physical, monster, mdam);
+			int res = monster.resistance & (RESIST_MAGIC | RESIST_FIRE | RESIST_LIGHTNING | IMMUNE_MAGIC | IMMUNE_FIRE | IMMUNE_LIGHTNING);
+			if ((res & (RESIST_FIRE || IMMUNE_FIRE)) != 0)
+				mdam = mdam / 2;
+			ApplyMonsterDamage(DamageType::Fire, monster, mdam);
 			if (monster.hitPoints >> 6 <= 0)
 				M_StartKill(monster, player);
 			else
@@ -1279,7 +1282,10 @@ void MonsterAttackPlayer(Monster &monster, Player &player, int hit, int minDam, 
 		eMind = player._pIFMinDam;
 		eMaxd = player._pIFMaxDam;
 		int mdam = (GenerateRnd(eMaxd) + eMind) << 6;
-		ApplyMonsterDamage(DamageType::Physical, monster, mdam);
+		int res = monster.resistance & (RESIST_MAGIC | RESIST_FIRE | RESIST_LIGHTNING | IMMUNE_MAGIC | IMMUNE_FIRE | IMMUNE_LIGHTNING);
+		if ((res & (RESIST_FIRE || IMMUNE_FIRE)) != 0)
+			mdam = mdam / 2;
+		ApplyMonsterDamage(DamageType::Fire, monster, mdam);
 		if (monster.hitPoints >> 6 <= 0)
 			M_StartKill(monster, player);
 		else
