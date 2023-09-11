54601e16a082408656152d42e8b861ccb7a7b473
wkdgmr <wkdoov@gmail.com>
2023-09-10 20:31:54 -0400

    LinuxReleasePackaging.sh fix for Github Actions

--- Packaging/nix/LinuxReleasePackaging.sh
+++ Packaging/nix/LinuxReleasePackaging.sh
@@ -10,28 +10,29 @@ PKG_PATH=("${BUILD_DIR}/_CPack_Packages/Linux/7Z/"wkdmod-*/)
 PKG_PATH="${PKG_PATH[@]}"
 PKG_PATH="${PKG_PATH%/}"
 
-cp "${PKG_PATH}/bin/devilutionx" "${BUILD_DIR}/package/devilutionx"
+cp "${PKG_PATH}/bin/wkdmod" "${BUILD_DIR}/package/wkdmod"
+
 if [[ -f "${PKG_PATH}/lib/discord_game_sdk.so" ]]; then
 	cp "${PKG_PATH}/lib/discord_game_sdk.so" "${BUILD_DIR}/package/"
-	cat <<'SH' > "${BUILD_DIR}/package/devilutionx.sh"
+	cat <<'SH' > "${BUILD_DIR}/package/wkdmod.sh"
 #!/bin/sh
 BASEDIR="$(dirname "$(realpath "$0")")"
-LD_LIBRARY_PATH="$BASEDIR" "$BASEDIR"/devilutionx
+LD_LIBRARY_PATH="$BASEDIR" "$BASEDIR"/wkdmod
 SH
-	chmod +x "${BUILD_DIR}/package/devilutionx.sh"
+	chmod +x "${BUILD_DIR}/package/wkdmod.sh"
 fi
 
-cp "${BUILD_DIR}/devilutionx" "${BUILD_DIR}/package/devilutionx"
-cp "${BUILD_DIR}/devilutionx.mpq" "${BUILD_DIR}/package/devilutionx.mpq"
+cp "${BUILD_DIR}/wkdmod" "${BUILD_DIR}/package/wkdmod"
+cp "${BUILD_DIR}/wkdmod.mpq" "${BUILD_DIR}/package/wkdmod.mpq"
 
 if which dpkg 2>/dev/null; then
-	cp "${BUILD_DIR}/"devilutionx*.deb "${BUILD_DIR}/package/devilutionx.deb"
+	cp "${BUILD_DIR}/"wkdmod*.deb "${BUILD_DIR}/package/wkdmod.deb"
 fi
 if which rpmbuild; then
-	cp "${BUILD_DIR}/"devilutionx*.rpm "${BUILD_DIR}/package/devilutionx.rpm"
+	cp "${BUILD_DIR}/"wkdmod*.rpm "${BUILD_DIR}/package/wkdmod.rpm"
 fi
 
 cp ./Packaging/nix/README.txt "${BUILD_DIR}/package/README.txt"
 cp ./Packaging/resources/LICENSE.CC-BY.txt "${BUILD_DIR}/package/LICENSE.CC-BY.txt"
 cp ./Packaging/resources/LICENSE.OFL.txt "${BUILD_DIR}/package/LICENSE.OFL.txt"
-cd "${BUILD_DIR}/package/" && tar -cavf ../../devilutionx.tar.xz *
+cd "${BUILD_DIR}/package/" && tar -cavf ../../wkdmod.tar.xz *
